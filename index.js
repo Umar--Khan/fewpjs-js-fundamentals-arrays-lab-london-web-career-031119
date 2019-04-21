8ac2b3f6fecdca8e6fbe77c0c82d06eab7c7ca47
Umar--Khan <umarkhandev@gmail.com>
2019-04-21 16:51:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import { appendFile } from "fs";
+
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+append.push("Odie");
+
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.shift("Odie");
+
+const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop();
+
+const removeFirst = ["Milo", "Otis", "Garfield"];
+removeFirst.shift();
+
